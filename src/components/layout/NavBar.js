40cf585d9d88d1e30053f9f4edf26772a817bd7d
rahp124 <punji.rahul@gmail.com>
2021-07-16 03:46:13 -0700

    testing new sprites

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -74,7 +74,7 @@ export default class NavBar extends Component {
             </li>
           </ul>
           <form className="form-inline my-2 my-lg-0 pull-right search-box">
-            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+            <input className="form-control mr-sm-2" type="search" placeholder="Search Pokemon" aria-label="Search"/>
             <button className="btn my-2 my-sm-0" type="submit">Search</button>
           </form>
         </div>    
